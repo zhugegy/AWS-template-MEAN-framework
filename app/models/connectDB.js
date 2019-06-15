71a063523e2edd8345c81e4169f7da3dc0290ef2
zgc <zhugechenw@gmail.com>
2019-06-15 22:25:03 +1000

    changed MongoDB to AWS EC2

--- app/models/connectDB.js
+++ app/models/connectDB.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/wikipedia', { useNewUrlParser: true }, function(){
+//mongoose.connect('mongodb://localhost/wikipedia', { useNewUrlParser: true }, function(){
+//    console.log('mongodb connected')
+//});
+
+mongoose.connect('mongodb://briskwikipedia:3201@ec2-3-219-35-26.compute-1.amazonaws.com:27017/wikipedia', { useNewUrlParser: true }, function(){
     console.log('mongodb connected')
 });
+
+//mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB
 
 module.exports = mongoose;
