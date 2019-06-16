de390750f39881e4591985f87af34e878fa757b6
zgc <zhugechenw@gmail.com>
2019-06-16 19:05:56 +1000

    applied reverse proxy test 7

--- reverse_proxy_server.js
+++ reverse_proxy_server.js
@@ -1,13 +1,39 @@
-var express  = require('express');
-var app      = express();
-var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
-var serverArticleInsights = 'http://3.212.186.203:3001';
+// var express  = require('express');
+// var app      = express();
+// var httpProxy = require('http-proxy');
+// var apiProxy = httpProxy.createProxyServer();
+// var serverArticleInsights = 'http://3.212.186.203:3001';
+//
+// app.all("/article_insights/*", function(req, res) {
+//     console.log('redirecting to serverArticleInsights');
+//     apiProxy.web(req, res, {target: serverArticleInsights, changeOrigin: true});
+// });
+//
+// app.listen(3000);
+//
 
-app.all("/article_insights/*", function(req, res) {
-    console.log('redirecting to serverArticleInsights');
-    apiProxy.web(req, res, {target: serverArticleInsights, changeOrigin: true});
-});
+// const httpProxy = require('http-proxy');
+// const http = require('http');
+//
+// const targetHost = 'www.google.co.uk';
+//
+// const proxy = httpProxy.createProxyServer({
+//     target: 'http://' + targetHost
+// });
+//
+// http.createServer(function (req, res) {
+//     proxy.web(req, res);
+//
+// }).listen(3000);
+//
+// proxy.on('proxyReq', function(proxyReq, req, res, options) {
+//     proxyReq.setHeader('Host', targetHost);
+// });
 
-app.listen(3000);
+var proxy = require('express-http-proxy');
+var app = require('express')();
 
+app.use('/article-insights', proxy('http://3.212.186.203:3001'));
+app.listen(3000, function () {
+    console.log("listening on port 3000!");
+})
