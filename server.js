94f027ab7d9b35154d605aa2fb4bd47a74640777
zgc <zhugechenw@gmail.com>
2019-06-16 15:23:27 +1000

    trying to deploy with domain

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.use('/', articleInsightRouter);
 //yyt
 app.use('/', logInPageRouter);
 
-app.listen(3000, function () {
-    console.log("listening on port 3000!");
+app.listen(80, function () {
+    console.log("listening on port 80!");
 })
