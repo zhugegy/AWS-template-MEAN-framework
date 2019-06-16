4a0d4ed456cc92afdc3f20d1f34a4bbf28ee10db
zgc <zhugechenw@gmail.com>
2019-06-16 18:11:53 +1000

    applied reverse proxy test 4

--- reverse_proxy_server.js
+++ reverse_proxy_server.js
@@ -1,16 +1,13 @@
-// var express  = require('express');
-// var app      = express();
-// var httpProxy = require('http-proxy');
-// var apiProxy = httpProxy.createProxyServer();
-// var serverArticleInsights = 'http://localhost:3001';
-//
-// app.all("/article_insights/*", function(req, res) {
-//     console.log('redirecting to serverArticleInsights');
-//     apiProxy.web(req, res, {target: serverArticleInsights});
-// });
-//
-// app.listen(3000);
+var express  = require('express');
+var app      = express();
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+var serverArticleInsights = 'http://3.212.186.203:3001';
 
-var proxy = require('redbird')({port: 3000});
+app.all("/article_insights/*", function(req, res) {
+    console.log('redirecting to serverArticleInsights');
+    apiProxy.web(req, res, {target: serverArticleInsights});
+});
 
-proxy.register("mutelookout.com/article_insights", "http://3.212.186.203:3001/");
+app.listen(3000);
+
